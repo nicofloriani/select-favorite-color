a8be08caa5e08e66f29465306188eb72290083a7
nico_floriani <floriani.nico@gmail.com>
2023-08-26 09:33:16 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Select your favorite color!</h2>
+      <div className="color-picker">
+        <p>Click me ▶▶▶</p>
+        <input className="color-input" type="color" />
+      </div>
+      <div className="color-square" style={{}}></div>
+    </div>
+  );
+};
+
+export default App;
